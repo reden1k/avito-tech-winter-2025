a5202d7ab8170f872a1946a23f2a68fc9fb3d744
reden <dim23062006@gmail.com>
2025-02-14 22:59:39 +0300

    added routing

--- main.go
+++ main.go
@@ -10,7 +10,10 @@ import (
 func main() {
 	models.InitDB()
 
-	http.HandleFunc("/auth", handlers.AuthHandler)
+	http.HandleFunc("/api/auth", handlers.AuthHandler)
+	http.HandleFunc("/api/info", handlers.InfoHandler)
+	http.HandleFunc("/api/buy/", handlers.BuyHandler)
+	http.HandleFunc("/api/sendCoin", handlers.SendCoinsHandler)
 
 	log.Println("Сервер запущен на порту 8080")
 	err := http.ListenAndServe(":8080", nil)
