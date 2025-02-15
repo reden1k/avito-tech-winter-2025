39f93e3dd66a577aa2e01755eec3eb9ca8df3bc2
reden <dim23062006@gmail.com>
2025-02-15 22:01:58 +0300

    optimized little bit

--- services/auth.go
+++ services/auth.go
@@ -19,77 +19,108 @@ func HandleAuthRequest(req dto.AuthRequest) (*dto.AuthResponse, *dto.Error) {
 		}
 	}
 
-	var id int
-	var hashedPassword string
-	err := db.DB.QueryRow("SELECT id, password_hash FROM employees WHERE username = $1", req.Username).
-		Scan(&id, &hashedPassword)
+	resultCh := make(chan *dto.AuthResponse)
+	errorCh := make(chan *dto.Error)
 
-	if err == sql.ErrNoRows {
-		return createUser(req.Username, req.Password)
-	} else if err != nil {
-		return nil, &dto.Error{
-			Code:       "DB_ERROR",
-			Message:    "Ошибка работы с базой данных",
-			StatusCode: 500,
+	go func() {
+		var id int
+		var hashedPassword string
+		err := db.DB.QueryRow("SELECT id, password_hash FROM employees WHERE username = $1", req.Username).
+			Scan(&id, &hashedPassword)
+
+		if err == sql.ErrNoRows {
+			response, err := createUser(req.Username, req.Password)
+			if err != nil {
+				errorCh <- err
+			} else {
+				resultCh <- response
+			}
+			return
+		} else if err != nil {
+			errorCh <- &dto.Error{
+				Code:       "DB_ERROR",
+				Message:    "Ошибка работы с базой данных",
+				StatusCode: 500,
+			}
+			return
 		}
-	}
 
-	// Проверяем пароль
-	if bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(req.Password)) != nil {
-		return nil, &dto.Error{
-			Code:       "INVALID_PASSWORD",
-			Message:    "Неверный пароль",
-			StatusCode: 401,
+		if bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(req.Password)) != nil {
+			errorCh <- &dto.Error{
+				Code:       "INVALID_PASSWORD",
+				Message:    "Неверный пароль",
+				StatusCode: 401,
+			}
+			return
 		}
-	}
 
-	// Генерируем JWT
-	tokenString, err := generateJWT(id)
-	if err != nil {
-		return nil, &dto.Error{
-			Code:       "TOKEN_GENERATION_ERROR",
-			Message:    "Ошибка генерации токена",
-			StatusCode: 500,
+		tokenString, err := generateJWT(id)
+		if err != nil {
+			errorCh <- &dto.Error{
+				Code:       "TOKEN_GENERATION_ERROR",
+				Message:    "Ошибка генерации токена",
+				StatusCode: 500,
+			}
+			return
 		}
-	}
 
-	return &dto.AuthResponse{Token: tokenString}, nil
+		resultCh <- &dto.AuthResponse{Token: tokenString}
+	}()
+
+	select {
+	case res := <-resultCh:
+		return res, nil
+	case err := <-errorCh:
+		return nil, err
+	}
 }
 
 func createUser(username, password string) (*dto.AuthResponse, *dto.Error) {
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MinCost)
-	if err != nil {
-		return nil, &dto.Error{
-			Code:       "PASSWORD_HASH_ERROR",
-			Message:    "Ошибка хеширования пароля",
-			StatusCode: 500,
+	hashCh := make(chan string)
+	errorCh := make(chan *dto.Error)
+
+	go func() {
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MinCost)
+		if err != nil {
+			errorCh <- &dto.Error{
+				Code:       "PASSWORD_HASH_ERROR",
+				Message:    "Ошибка хеширования пароля",
+				StatusCode: 500,
+			}
+			return
 		}
-	}
+		hashCh <- string(hashedPassword)
+	}()
 
-	var id int
-	err = db.DB.QueryRow(
-		"INSERT INTO employees (username, password_hash, coins) VALUES ($1, $2, $3) RETURNING id",
-		username, string(hashedPassword), 1000,
-	).Scan(&id)
+	select {
+	case hashedPassword := <-hashCh:
+		var id int
+		err := db.DB.QueryRow(
+			"INSERT INTO employees (username, password_hash, coins) VALUES ($1, $2, $3) RETURNING id",
+			username, hashedPassword, 1000,
+		).Scan(&id)
 
-	if err != nil {
-		return nil, &dto.Error{
-			Code:       "USER_CREATION_ERROR",
-			Message:    "Ошибка создания пользователя",
-			StatusCode: 500,
+		if err != nil {
+			return nil, &dto.Error{
+				Code:       "USER_CREATION_ERROR",
+				Message:    "Ошибка создания пользователя",
+				StatusCode: 500,
+			}
 		}
-	}
 
-	tokenString, err := generateJWT(id)
-	if err != nil {
-		return nil, &dto.Error{
-			Code:       "TOKEN_GENERATION_ERROR",
-			Message:    "Ошибка генерации токена",
-			StatusCode: 500,
+		tokenString, err := generateJWT(id)
+		if err != nil {
+			return nil, &dto.Error{
+				Code:       "TOKEN_GENERATION_ERROR",
+				Message:    "Ошибка генерации токена",
+				StatusCode: 500,
+			}
 		}
-	}
 
-	return &dto.AuthResponse{Token: tokenString}, nil
+		return &dto.AuthResponse{Token: tokenString}, nil
+	case err := <-errorCh:
+		return nil, err
+	}
 }
 
 func generateJWT(userId int) (string, error) {
